98affb1ccc005fa6a98be83020a824a28ce7e5ee
Hinekureta <herrbach.kevin@gmail.com>
2017-09-15 19:31:30 +0200

    Finishes question 1, added some comments above the profileAction function

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,18 +2,28 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Friends;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
+     * [QUESTION 1] I tried multiple ways to count the number of friends following a
+     * particular id.
+     * 1) Using findBy from Doctrine
+     * 2) Using a DQL inside a Repository
+     * 3) Using a native query
+     * All got approximatly the same query time so I kept the simple one (1).
+     * Did not know what to return, a page or a simple JSON. Decided to return a page
+     * and added the count as a variable.
      * @Route("/user/{id}", name="profile")
      */
     public function profileAction($id)
     {
-
+        $count = count($this->getDoctrine()->getRepository(Friends::class)
+        ->findByFollowing($id));
+        return $this->render('base.html.twig', ['count' => $count]);
     }
 
     /**
